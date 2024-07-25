d6fe8113500d3ff7e332e0dd56adcbd5201773ff
Saem <fornared@gmail.com>
2024-07-25 12:43:48 +0900

    도서 테이블 생성

--- back-end/sql/project.sql
+++ back-end/sql/project.sql
@@ -19,3 +19,28 @@ CREATE TABLE member_authority
     member_id INT         NOT NULL REFERENCES member (id),
     name      VARCHAR(10) NOT NULL
 );
+
+CREATE TABLE book
+(
+    id               INT PRIMARY KEY AUTO_INCREMENT,
+    category_id      INT          NOT NULL REFERENCES category (id),
+    title            VARCHAR(255) NOT NULL,
+    author           VARCHAR(100) NOT NULL,
+    publisher        VARCHAR(100) NOT NULL,
+    publication_year VARCHAR(4),
+    isbn             VARCHAR(13)  NOT NULL,
+    description      TEXT,
+    inserted         DATETIME     NOT NULL DEFAULT NOW()
+);
+CREATE TABLE category
+(
+    id   INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+CREATE TABLE book_image
+(
+    id      INT PRIMARY KEY AUTO_INCREMENT,
+    book_id INT NOT NULL REFERENCES book (id),
+    name    VARCHAR(255),
+    url     VARCHAR(1024)
+);
